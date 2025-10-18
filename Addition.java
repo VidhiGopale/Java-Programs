67bf24ff0e2368dce1fbfe9c4ed29a668e90f8c0
VidhiGopale <vidhigopale@gmail.com>
2025-10-18 23:49:36 +0530

    Initial Commit of Java Programs

---
+++ Addition.java
@@ -0,0 +1,9 @@
+//Write a program to perform addition in java
+public class Addition{
+    public static void main (String[] args){
+    int a=23;
+    int b=10;
+    int sum=a+b;
+    System.out.println("The addition of numbers is " + sum);
+    }
+}
