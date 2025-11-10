a650cda4e2bc2a63991183584dbe93e67f8e256f
VidhiGopale <vidhigopale@gmail.com>
2025-11-10 22:33:45 +0530

    Adding java basic practice questions

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -5,11 +5,15 @@ public class ArmstrongNumber{
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the number: ");
         int a=sc.nextInt();
+        int num=a;
         int sum=0;
+        int length=String.valueOf(a).length();
         while(a>0){
-            
+            int digit=a%10;
+            sum+=Math.pow(digit, length);
+            a=a/10;
         }
-        if(sum==a){
+        if(sum==num){
             System.out.println("Yes, this is the armstrong number!");
         }
         else{
